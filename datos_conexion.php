b04a383f0ac0f5e2481e80cd5ca289000783eb19
aguirre10 <aguirrejhensser@gmail.com>
2021-09-21 15:12:47 -0600

    Subiendo datos de conexion a BD

---
+++ datos_conexion.php
@@ -0,0 +1,8 @@
+<!-- Datos de Conexion a Base de Datos MySql-->
+<?php 
+    $db_host = "localhost";
+    $db_usr = "root";
+    $db_pass = "Jhensser10*";
+    $db_name = "segundoparcial";
+    $db_port = "3306";
+?>
